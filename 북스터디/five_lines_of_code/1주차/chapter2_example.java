2e261fd3b19ce45a691e43eb3d5167dbb82b8b47
SeolYoungKim <nasur4da@gmail.com>
2023-09-19 20:59:02 +0900

    feat: 상속과 컴포지션의 차이점 작성

--- 북스터디/five_lines_of_code/1주차/chapter2_example.java
+++ 북스터디/five_lines_of_code/1주차/chapter2_example.java
@@ -47,7 +47,7 @@ class CommonBird implements Bird {
     }
 }
 
-class Penguin extends CommonBird {
+class Penguin extends CommonBird {  // Bird 인터페이스에 새로운 메서드가 추가될 경우 CommonBird만 컴파일 에러가 발생하며, Penguin에는 컴파일 에러가 발생하지 않음 -> 수정해야 함을 놓칠 수 있음 -> 심각한 에러 유발
     public boolean canFly() {  // 범위가 제한되지 않아 상속을 통해 얼마든지 변경될 수 있다.
         return false;
     }
@@ -56,7 +56,7 @@ class Penguin extends CommonBird {
 /**
  * 컴포지션을 이용하는 방법
  */
-class Penguin implements Bird {
+class Penguin implements Bird {  // Bird 인터페이스에 새로운 메서드가 추가될 경우 컴파일 에러 발생 -> Penguin도 수정해야 함을 알 수 있음
     Bird bird = new CommonBird();
 
     boolean hasBeak() {
