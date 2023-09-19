8a7b2c0377b6c894283ac285eebcb183fb812b60
SeolYoungKim <nasur4da@gmail.com>
2023-09-19 20:54:34 +0900

    feat: 상속을 통해 범위가 제한되지 않은 불변 속성 예제 구현

--- 북스터디/five_lines_of_code/1주차/chapter2_example.java
+++ 북스터디/five_lines_of_code/1주차/chapter2_example.java
@@ -1,4 +1,4 @@
-class Main {
+class Refactoring1 {
     // before
     boolean checkValue(boolean str) {
         // 값 체크
@@ -22,5 +22,31 @@ class Main {
     // 단순화
     boolean isTrue(boolean bool) {
         return bool;
+    }
+}
+
+/**
+ * 상속
+ */
+interface Bird {
+    boolean hasBeak();
+
+    boolean canFly();
+}
+
+class CommonBird implements Bird {
+    boolean hasBeak() {
+        return true;
+    }
+
+    boolean canFly() {  // 범위가 제한되지 않은 불변 속성
+        return true;
+    }
+}
+
+class Penguin extends CommonBird {
+    @Override
+    public boolean canFly() {  // 범위가 제한되지 않아 상속을 통해 얼마든지 변경될 수 있다.
+        return false;
     }
 }
