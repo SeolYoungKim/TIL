fe200aa0824f14c97c98786eb75c65fa94c59aaa
SeolYoungKim <nasur4da@gmail.com>
2023-09-20 21:59:27 +0900

    feat: 예제 코드 작성

---
+++ 북스터디/five_lines_of_code/1주차/chapter3_example.java
@@ -0,0 +1,43 @@
+class Game {
+    void draw() {
+        var canvas = getCanvas();
+        var g = canvas.getContext("2d");
+
+        g.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Draw map
+        drawMap();
+
+        // Draw player
+        drawPlayer();
+    }
+
+    void drawMap() {
+        for (let y = 0; y < map.length; y++) {
+            for (let x = 0; x < map[y].length; x++) {
+                if (map[y][x] == Tile.FLUX) {
+                    g.fillStyle = "#ccffcc";
+                } else if (map[y][x] == Tile.UNBREAKABLE) {
+                    g.fillStyle = "#999999";
+                } else if (map[y][x] == Tile.STONE || map[y][x] == Tile.FALLING_STONE) {
+                    g.fillStyle = "#0000cc";
+                } else if (map[y][x] == Tile.BOX || map[y][x] == Tile.FALLING_BOX) {
+                    g.fillStyle = "#8b4513";
+                } else if (map[y][x] == Tile.KEY1 || map[y][x] == Tile.LOCK1) {
+                    g.fillStyle = "#ffcc00";
+                } else if (map[y][x] == Tile.KEY2 || map[y][x] == Tile.LOCK2) {
+                    g.fillStyle = "#00ccff";
+                }
+
+                if (map[y][x] != Tile.AIR && map[y][x] != Tile.PLAYER) {
+                    g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                }
+            }
+        }
+    }
+
+    void drawPlayer() {
+        g.fillStyle = "#ff0000";
+        g.fillRect(playerx * TILE_SIZE, playery * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    }
+}
