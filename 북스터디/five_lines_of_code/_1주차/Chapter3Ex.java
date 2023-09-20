e20daeffa5df5b5f9de1d4deb6efb36a4eb2c808
SeolYoungKim <nasur4da@gmail.com>
2023-09-20 22:26:10 +0900

    refactor: 공통 변수 분리

--- 북스터디/five_lines_of_code/_1주차/Chapter3Ex.java
+++ 북스터디/five_lines_of_code/_1주차/Chapter3Ex.java
@@ -5,17 +5,16 @@ class Chapter3Ex {
         int result = Integer.MAX_VALUE;
         for (int x = 0; x < numbers.length; x++) {
             for (int y = 0; y < numbers[x].length; y++) {
-
-                result = getMinNum(numbers, result, x, y);
+                result = getMinNum(result, numbers[x][y]);
             }
         }
 
         return result;
     }
 
-    private int getMinNum(int[][] numbers, int result, int x, int y) {
-        if (result > numbers[x][y]) {
-            result = numbers[x][y];
+    private int getMinNum(int result, int targetNumber) {
+        if (result > targetNumber) {
+            result = targetNumber;
         }
 
         return result;
