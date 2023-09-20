22c67a359f9978b339375418ad473c453a98c206
SeolYoungKim <nasur4da@gmail.com>
2023-09-20 22:17:41 +0900

    feat: 배열의 최소 항목을 찾는 함수

--- 북스터디/five_lines_of_code/_1주차/Chapter3Ex.java
+++ 북스터디/five_lines_of_code/_1주차/Chapter3Ex.java
@@ -1,6 +1,17 @@
 package 북스터디.five_lines_of_code._1주차;
 
-import java.awt.Canvas;
-
 class Chapter3Ex {
+    int findMinNum(int[][] numbers) {
+        int result = Integer.MAX_VALUE;
+        for (int x = 0; x < numbers.length; x++) {
+            for (int y = 0; y < numbers[x].length; y++) {
+
+                if (result > numbers[x][y]) {
+                    result = numbers[x][y];
+                }
+            }
+        }
+
+        return result;
+    }
 }
