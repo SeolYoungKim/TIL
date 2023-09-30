8efb8b3dc768224ce9a7ccbd8ae311f51a955630
SeolYoungKim <nasur4da@gmail.com>
2023-09-30 20:47:28 +0900

    refactor: 클래스로 코드 이관하기 - if, else를 제거하기

--- 북스터디/five_lines_of_code/_2주차/Input.java
+++ 북스터디/five_lines_of_code/_2주차/Input.java
@@ -2,140 +2,29 @@ package 북스터디.five_lines_of_code._2주차;
 
 public enum Input {
     LEFT(){
-        void handleInput(Input input) {
-            if (input.isLeft()) {
-                moveHorizontal(-1);
-            } else if (input.isRight()) {
-                moveHorizontal(1);
-            } else if (input.isUp()) {
-                moveVertical(-1);
-            } else if (input.isDown()) {
-                moveVertical(1);
-            }
-        }
-
         @Override
-        boolean isLeft() {
-            return true;
-        }
-
-        @Override
-        boolean isRight() {
-            return false;
-        }
-
-        @Override
-        boolean isUp() {
-            return false;
-        }
-
-        @Override
-        boolean isDown() {
-            return false;
+        void handle() {
+            moveHorizontal(-1);
         }
     },
     RIGHT(){
-        void handleInput(Input input) {
-            if (input.isLeft()) {
-                moveHorizontal(-1);
-            } else if (input.isRight()) {
-                moveHorizontal(1);
-            } else if (input.isUp()) {
-                moveVertical(-1);
-            } else if (input.isDown()) {
-                moveVertical(1);
-            }
-        }
-
         @Override
-        boolean isLeft() {
-            return false;
-        }
-
-        @Override
-        boolean isRight() {
-            return true;
-        }
-
-        @Override
-        boolean isUp() {
-            return false;
-        }
-
-        @Override
-        boolean isDown() {
-            return false;
+        void handle() {
+            moveHorizontal(1);
         }
     },
     UP(){
-        void handleInput(Input input) {
-            if (input.isLeft()) {
-                moveHorizontal(-1);
-            } else if (input.isRight()) {
-                moveHorizontal(1);
-            } else if (input.isUp()) {
-                moveVertical(-1);
-            } else if (input.isDown()) {
-                moveVertical(1);
-            }
-        }
-
         @Override
-        boolean isLeft() {
-            return false;
-        }
-
-        @Override
-        boolean isRight() {
-            return false;
-        }
-
-        @Override
-        boolean isUp() {
-            return true;
-        }
-
-        @Override
-        boolean isDown() {
-            return false;
+        void handle() {
+            moveVertical(-1);
         }
     },
     DOWN(){
-        void handleInput(Input input) {
-            if (input.isLeft()) {
-                moveHorizontal(-1);
-            } else if (input.isRight()) {
-                moveHorizontal(1);
-            } else if (input.isUp()) {
-                moveVertical(-1);
-            } else if (input.isDown()) {
-                moveVertical(1);
-            }
-        }
-
         @Override
-        boolean isLeft() {
-            return false;
-        }
-
-        @Override
-        boolean isRight() {
-            return false;
-        }
-
-        @Override
-        boolean isUp() {
-            return false;
-        }
-
-        @Override
-        boolean isDown() {
-            return true;
+        void handle() {
+            moveVertical(1);
         }
     },;
 
-    abstract boolean isLeft();
-    abstract boolean isRight();
-    abstract boolean isUp();
-    abstract boolean isDown();
+    abstract void handle();
 }
